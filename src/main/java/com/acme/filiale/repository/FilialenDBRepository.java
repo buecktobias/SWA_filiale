7a1c7a088da6e13a889e4a06cdaabfa5efa3772b
Tobias Bück <buecktobias@gmail.com>
2023-01-13 15:42:19 +0100

    added mysql

--- src/main/java/com/acme/filiale/repository/FilialenDBRepository.java
+++ src/main/java/com/acme/filiale/repository/FilialenDBRepository.java
@@ -2,9 +2,10 @@ package com.acme.filiale.repository;
 
 import com.acme.filiale.entity.Filiale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 @Repository
-public interface FilialenDBRepository extends JpaRepository<Filiale, Long> {
+public interface FilialenDBRepository extends JpaRepository<Filiale, Long>, JpaSpecificationExecutor<Filiale> {
 }
